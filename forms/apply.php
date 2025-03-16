c05ec566363bb62155cd7502c8e90ff59684a3d5
sunoh-kim <suno8386@snu.ac.kr>
2025-03-17 02:29:46 +0900

    add people

--- forms/apply.php
+++ forms/apply.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'suno8386@dankook.ac.kr';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -15,17 +15,17 @@
     die( 'Unable to load the "PHP Email Form" Library!');
   }
 
-  $book_a_table = new PHP_Email_Form;
-  $book_a_table->ajax = true;
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
   
-  $book_a_table->to = $receiving_email_address;
-  $book_a_table->from_name = $_POST['name'];
-  $book_a_table->from_email = $_POST['email'];
-  $book_a_table->subject = "New table booking request from the website";
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
-  $book_a_table->smtp = array(
+  $contact->smtp = array(
     'host' => 'example.com',
     'username' => 'example',
     'password' => 'pass',
@@ -33,13 +33,9 @@
   );
   */
 
-  $book_a_table->add_message( $_POST['name'], 'Name');
-  $book_a_table->add_message( $_POST['email'], 'Email');
-  $book_a_table->add_message( $_POST['phone'], 'Phone', 4);
-  $book_a_table->add_message( $_POST['date'], 'Date', 4);
-  $book_a_table->add_message( $_POST['time'], 'Time', 4);
-  $book_a_table->add_message( $_POST['people'], '# of people', 1);
-  $book_a_table->add_message( $_POST['message'], 'Message');
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
 
-  echo $book_a_table->send();
+  echo $contact->send();
 ?>
